25ec0800b79698f78711d1dcc7603e745009f816
Jhyoon <face5921@naver.com>
2024-05-01 16:24:00 +0900

    gitignore

--- source image/camera_cap.py
+++ source image/camera_cap.py
@@ -3,7 +3,7 @@ import os
 import time
 from datetime import datetime
 
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(1)
 capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 save_images = []
@@ -12,20 +12,22 @@ save_images = []
 ########################
     # 이미지 캡쳐 #
 ########################
-
+cur_time = time.time()
 while capture.isOpened(): 
     ret, img = capture.read()           # 다음 프레임 읽기
     if ret:                         # 프레임 읽기 정상
         cv2.imshow('camera', img)
-        save_images.append(img)
+        if time.time() - cur_time > 0.5:
+            save_images.append(img)
+            cur_time = time.time()
     else:
         print('no frame')           # 다음 프레임을 읽을 수 없음.
         break
-
+    
     if cv2.waitKey(1) == ord('q'): 
         break
 
-    time.sleep(0.5)
+    # time.sleep(0.5)
 
 capture.release()                           # 캡쳐 자원 반납
 cv2.destroyAllWindows()
@@ -34,9 +36,10 @@ cv2.destroyAllWindows()
 ########################
     # 이미지 저장 #
 ########################
-
+current_path = os.getcwd()
+print(current_path)
 try:
-    os.mkdir('images')
+    os.mkdir(current_path + '\images')
 except Exception as ex:
     print('images folder already make it', ex)
 
